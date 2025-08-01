a645e0a534460c33e64596867432d6da46a66717
codex-yasxh <adit7ya0patanwar@outlook.com>
2025-08-01 17:18:36 +0530

    commits for stack nav

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+
+
+export const SCREENS = {
+    Splash : 'Splash',
+    Login : 'Login',
+    Home : 'Home',
+    SignUp : 'SignUp',
+    Forgot : 'ForgotPassword' //left side ones are the names which will be used when navigation or inside JSX like {SCREENS.Forgot} means Forgotpass becomes forgot 
+};
