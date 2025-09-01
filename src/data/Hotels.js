e4ecf834a9308c74ff902740dd23c535414c15f6
codex-yasxh <adit7ya0patanwar@outlook.com>
2025-09-01 10:00:05 +0530

    Built menu screens

--- src/data/Hotels.js
+++ src/data/Hotels.js
@@ -14,6 +14,35 @@ const hotels = [
     latitude: 12.9716,
     longitude: 77.5946,
     time: "43 min",
+    menu: [
+      {
+        id: "1",
+        name: "Amritsari Fish Fry",
+        price: 280,
+        rating: 4.6,
+        description: "A classic dish that feels like a warm hug.",
+        badge: "bestseller",
+        image: "https://images.unsplash.com/photo-1656389863341-1dfd38ee6edc?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        id: "2",
+        name: "Chicken Malai Tikka",
+        price: 320,
+        rating: 4.4,
+        description: "Indulge in a culinary delight crafted with the finest ingredients.",
+        badge: "Must try",
+        image: "https://plus.unsplash.com/premium_photo-1675864532625-60efd11cde54?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        id: "3",
+        name: "Butter Chicken",
+        price: 350,
+        rating: 4.7,
+        description : "",
+        badge: "chef special",
+        image: "https://images.unsplash.com/photo-1728910107534-e04e261768ae?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      }
+    ]
   },
   {
     id: "2",
@@ -28,6 +57,34 @@ const hotels = [
     latitude: 28.4595,
     longitude: 77.0266,
     time: "32 min",
+    menu: [
+      {
+        id: "1",
+        name: "Filter Coffee",
+        price: 80,
+        rating: 4.8,
+        description: "A classic dish that feels like a warm hug.",
+        badge: "bestseller",
+        image: "https://images.unsplash.com/photo-1587955245893-389f2215c6eb?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        id: "2",
+        name: "Masala Dosa",
+        price: 150,
+        rating: 4.5,
+        description: "Sometimes the best things are the simplest. Enjoy a timeless favorite.",
+        badge: "Must try",
+        image: "https://images.unsplash.com/photo-1668236543090-82eba5ee5976?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dhttps://images.unsplash.com/photo-1668236543090-82eba5ee5976?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        id: "3",
+        name: "Veg Cutlet",
+        price: 120,
+        rating: 4.3,
+        badge: "bestseller",
+        image: "https://images.unsplash.com/photo-1598298125619-ac63f5c89d90?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      }
+    ]
   },
   {
     id: "3",
@@ -42,6 +99,32 @@ const hotels = [
     latitude: 12.9719,
     longitude: 77.6412,
     time: "27 min",
+    menu: [
+      {
+        id: "1",
+        name: "Pepperoni Pizza",
+        price: 399,
+        rating: 4.6,
+        badge: "bestseller",
+        image: "https://images.unsplash.com/photo-1705537637301-956413896f3c?q=80&w=789&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        id: "2",
+        name: "Veggie Supreme",
+        price: 349,
+        rating: 4.4,
+        badge: "Must try",
+        image: "https://images.unsplash.com/photo-1681567604770-0dc826c870ae?q=80&w=900&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        id: "3",
+        name: "Cheese Garlic Bread",
+        price: 189,
+        rating: 4.5,
+        badge: "bestseller",
+        image: "https://images.unsplash.com/photo-1709203876602-f23c63fff81b?q=80&w=2009&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      }
+    ]
   },
   {
     id: "4",
@@ -56,6 +139,32 @@ const hotels = [
     latitude: 28.6353,
     longitude: 77.2249,
     time: "35 min",
+    menu: [
+      {
+        id: "1",
+        name: "Raj Kachori",
+        price: 40,
+        rating: 4.6,
+        badge: "bestseller",
+        image: "https://images.unsplash.com/photo-1616787700988-44c85b2ffcf5?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        id: "2",
+        name: "Pav Bhaji",
+        price: 60,
+        rating: 4.5,
+        badge: "Must try",
+        image: "https://images.unsplash.com/photo-1606491956689-2ea866880c84?q=80&w=1021&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        id: "3",
+        name: "Chole Bhature",
+        price: 180,
+        rating: 4.7,
+        badge: "chef special",
+        image: "https://cdn.s3waas.gov.in/s33416a75f4cea9109507cacd8e2f2aefc/uploads/bfi_thumb/2019082662-olw83cmkaiz8eb77zl4k8owpfszt7qdzuc9ae2gurq.jpg"
+      }
+    ]
   },
   {
     id: "5",
@@ -70,6 +179,32 @@ const hotels = [
     latitude: 12.9352,
     longitude: 77.6245,
     time: "30 min",
+    menu: [
+      {
+        id: "1",
+        name: "Tandoori Chicken",
+        price: 320,
+        rating: 4.6,
+        badge: "bestseller",
+        image: "https://images.unsplash.com/photo-1628294896516-344152572ee8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        id: "2",
+        name: "Chicken Seekh Kebab",
+        price: 280,
+        rating: 4.5,
+        badge: "Must try",
+        image: "https://images.unsplash.com/photo-1605908580297-f3e1c02e64ff?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      },
+      {
+        id: "3",
+        name: "Butter Naan",
+        price: 50,
+        rating: 4.4,
+        badge: "bestseller",
+        image: "https://images.unsplash.com/photo-1697155406014-04dc649b0953?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      }
+    ]
   },
 ];
 
--- src/screens/Hotels/HotelScreen.tsx
+++ src/screens/Hotels/HotelScreen.tsx
@@ -1,16 +1,56 @@
-import { StyleSheet, Text, View, Image, ScrollView, Dimensions, SafeAreaView } from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Dimensions,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+import React, { useState } from 'react';
 import { useRoute } from '@react-navigation/native';
+import { FlatList } from 'react-native-gesture-handler';
+import hotels from '../../data/Hotels';
 
 const HotelScreen = () => {
   const route = useRoute();
-  const { hotel } = route.params; 
-  const screenWidth = Dimensions.get("window").width;
+  const { hotel } = route.params;
+  const screenWidth = Dimensions.get('window').width;
+  const [quantities, setQuantities] = useState({});
+
+  //Increasing values of items in cart 
+
+  const increaseQty = (id) =>{
+    console.log("added item in the cart");
+    setQuantities((previous)=>{
+      let  current = previous[id] || 0;
+      let updated = current + 1;
+      return {...previous, [id]: updated} //if writing return then don't use () else use () no return sttmnt needed 
+    }
+    );
+  }
+
+  const decreaseQty = (id) =>{
+    console.log("removed item from the cart");
+    setQuantities((previous)=>{
+      let current = previous[id] || 0 ;
+      if(current > 0){
+        let updated = current - 1 ;
+        return { ...previous , [id] : updated}
+      }
+      return{
+        previous
+      }
+    }
+    )
+  }
+
+
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-        
         {/* Hero Image */}
         <Image
           source={{ uri: hotel.featured_image }}
@@ -50,10 +90,58 @@ const HotelScreen = () => {
 
         {/* Extra Details */}
         <View style={styles.extraDetails}>
-          <Text style={styles.delivery}>{hotel.no_of_Delivery}+ successful deliveries 🚀</Text>
+          <Text style={styles.delivery}>
+            {hotel.no_of_Delivery}+ successful deliveries 🚀
+          </Text>
           <Text style={styles.fee}>₹30 additional distance fee (if far)</Text>
         </View>
 
+        <View style={styles.menu}>
+          <FlatList
+            data={hotel.menu}
+            keyExtractor={menuItem => menuItem.id}
+            renderItem={({ item: menuItem }) => (
+              <View style={styles.card}>
+                {/* LEFT SIDE INFO */}
+                <View style={styles.cardInfo}>
+                  <Text style={styles.menuTitle}>{menuItem.name}</Text>
+                  <Text style={styles.menuDescription}>
+                    {menuItem.description || 'Delicious and freshly prepared'}
+                  </Text>
+                  <Text style={styles.menuPrice}>₹{menuItem.price}</Text>
+                  <Text style={styles.menuRating}>{menuItem.rating}⭐</Text>
+                </View>
+
+                {/* RIGHT SIDE IMAGE + BUTTON */}
+                <View style={styles.cardRight}>
+                  <Image
+                    source={{ uri: menuItem.image }}
+                    style={styles.menuImage}
+                  />
+                  <View style={styles.qtyContainer}>
+                    <TouchableOpacity
+                      style={styles.qtyButton}
+                      onPress={() => decreaseQty(menuItem.id)}
+                    >
+                      <Text style={styles.qtyText}>-</Text>
+                    </TouchableOpacity>
+
+                    <Text style={styles.qtyValue}>
+                      {quantities[menuItem.id] || 0}
+                    </Text>
+
+                    <TouchableOpacity
+                      style={styles.qtyButton}
+                      onPress={() => increaseQty(menuItem.id)}
+                    >
+                      <Text style={styles.qtyText}>+</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            )}
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -64,70 +152,70 @@ export default HotelScreen;
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
   heroImage: {
-    width: "100%",
-    height: Dimensions.get("window").width * 9/16,
+    width: '100%',
+    height: (Dimensions.get('window').width * 9) / 16,
     borderBottomLeftRadius: 16,
     borderBottomRightRadius: 16,
   },
   infoContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     padding: 16,
     borderBottomWidth: 1,
-    borderBottomColor: "#eee",
+    borderBottomColor: '#eee',
   },
   name: {
     fontSize: 22,
-    fontWeight: "700",
-    color: "#222",
+    fontWeight: '700',
+    color: '#222',
     marginBottom: 4,
   },
   cuisines: {
     fontSize: 15,
-    color: "#777",
+    color: '#777',
     marginBottom: 4,
   },
   address: {
     fontSize: 14,
-    color: "#555",
+    color: '#555',
   },
   badgeContainer: {
-    alignItems: "flex-end",
-    justifyContent: "center",
+    alignItems: 'flex-end',
+    justifyContent: 'center',
   },
   ratingBadge: {
-    backgroundColor: "green",
-    color: "#fff",
-    fontWeight: "600",
+    backgroundColor: 'green',
+    color: '#fff',
+    fontWeight: '600',
     paddingVertical: 4,
     paddingHorizontal: 8,
     borderRadius: 8,
     marginBottom: 6,
   },
   photoBadge: {
-    backgroundColor: "deeppink",
-    color: "#fff",
-    fontWeight: "600",
+    backgroundColor: 'deeppink',
+    color: '#fff',
+    fontWeight: '600',
     paddingVertical: 4,
     paddingHorizontal: 8,
     borderRadius: 8,
   },
   quickInfoRow: {
-    flexDirection: "row",
-    justifyContent: "space-around",
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     marginVertical: 12,
     paddingHorizontal: 8,
   },
   quickInfoBox: {
-    alignItems: "center",
-    backgroundColor: "#f7f7f7",
+    alignItems: 'center',
+    backgroundColor: '#f7f7f7',
     padding: 12,
     borderRadius: 12,
     flex: 1,
@@ -136,31 +224,115 @@ const styles = StyleSheet.create({
   },
   quickTitle: {
     fontSize: 13,
-    color: "#666",
+    color: '#666',
     marginBottom: 4,
   },
   quickValue: {
     fontSize: 15,
-    fontWeight: "600",
-    color: "#222",
+    fontWeight: '600',
+    color: '#222',
   },
   extraDetails: {
     paddingHorizontal: 16,
     paddingVertical: 12,
     borderTopWidth: 1,
-    borderTopColor: "#eae7e7ff",
-    backgroundColor : "#f8f8f8",
-    marginHorizontal : 8,
-    marginTop : 10,
-    borderRadius : 8
+    borderTopColor: '#eae7e7ff',
+    backgroundColor: '#f8f8f8',
+    marginHorizontal: 8,
+    marginTop: 10,
+    borderRadius: 8,
   },
   delivery: {
     fontSize: 15,
-    color: "#444",
+    color: '#444',
     marginBottom: 4,
   },
   fee: {
     fontSize: 13,
-    color: "#888",
+    color: '#888',
+  },
+  menu: {
+    paddingHorizontal: 16,
+    marginTop: 12,
+  },
+  card: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    backgroundColor: '#fff',
+    padding: 16,
+    marginBottom: 16,
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOpacity: 0.05,
+    shadowRadius: 5,
+    elevation: 2,
+  },
+  cardInfo: {
+    flex: 1,
+    paddingRight: 12,
+  },
+  menuTitle: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#222',
+    marginBottom: 4,
+  },
+  menuDescription: {
+    fontSize: 14,
+    color: '#777',
+    marginBottom: 6,
+  },
+  menuPrice: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  cardRight: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  menuImage: {
+    width: 90,
+    height: 90,
+    borderRadius: 10,
+    marginBottom: 8,
+  },
+  addButton: {
+    backgroundColor: '#0aada8',
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: '600',
+    paddingVertical: 6,
+    paddingHorizontal: 18,
+    borderRadius: 20,
+    overflow: 'hidden',
+  },
+  menuRating: {
+    fontSize: 12,
+    fontWeight: 500,
+    color: '#333',
+  },
+  qtyContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#ff4d6d',
+    borderRadius: 10,
+    paddingHorizontal: 6,
+    paddingVertical: 2,
+  },
+  qtyButton: {
+    paddingHorizontal: 10,
+  },
+  qtyText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '700',
+  },
+  qtyValue: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+    marginHorizontal: 6,
   },
 });
